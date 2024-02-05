1d89cfa6322af0a612bc3eb13a7d4a61c154a0ac
Me <marko99thebest@gmail.com>
2024-02-05 02:55:31 +0100

    la jvm e` molto poco affidabile nel prendere tempi, run diverse danno risultati molto diversi

--- src/main/scala/Benchmarks.scala
+++ src/main/scala/Benchmarks.scala
@@ -1,37 +1,33 @@
-import matrix.{AAMatrix, AMatrix}
+import matrix.{AAMatrix, NewAAMatrix}
+import tools.Timings
 import scala.util.Random
 import scala.math.pow
 
 object Benchmarks {
 	def start(args: Array[String]): Unit = {
-		val SIZE = 10
+		val SIZE = 512
 		val r = Random
 		val a = (0 until pow(SIZE.toDouble, 2).toInt).map(a => r.nextDouble()).toArray
 		val b = (0 until pow(SIZE.toDouble, 2).toInt).map(a => r.nextDouble()).toArray
+		
+		println("im about to start NewAAMatrix multiplication")
+		Thread.sleep(2000)
+		Timings.time("NewAAMatrix", {
+			val Aa = NewAAMatrix(SIZE, a.grouped(SIZE).toArray)
+			val Ab = NewAAMatrix(SIZE, b.grouped(SIZE).toArray)
+			val Ac = Aa multiply Ab
+			//println(Ac.toString)
+		})
 
 		println("im about to start AAMatrix multiplication")
 		Thread.sleep(2000)
-		val AAa = AAMatrix(a.grouped(SIZE).toArray.asInstanceOf[Array[Array[Double]]])
-		val AAb = AAMatrix(b.grouped(SIZE).toArray.asInstanceOf[Array[Array[Double]]])
+		Timings.time("AAMatrix", {
+			val Aa = AAMatrix(a.grouped(SIZE).toArray)
+			val Ab = AAMatrix(b.grouped(SIZE).toArray)
+			val Ac = Aa multiply Ab
+			//println(Ac.toString)
+		})
 
-		val AAbefore = System.currentTimeMillis()
-		val AAc = AAa multiply AAb
-		val AAafter = System.currentTimeMillis()
-		println("AAMatrix moltiplication, taken:", AAafter - AAbefore)
-		println(AAc.toString)
-		System.gc()
-
-		println("im about to start AMatrix multiplication")
-		Thread.sleep(2000)
-		val Abefore = System.currentTimeMillis()
-		val Aa = AMatrix(SIZE, a)
-		val Ab = AMatrix(SIZE, b)
-		println("created")
-		
-		val Ac = Aa multiply Ab
-		val Aafter = System.currentTimeMillis()
-		println("AMatrix moltiplication, taken:", Aafter - Abefore)
-		println(Ac.toString)
 	
   }
 }
